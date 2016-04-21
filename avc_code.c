f2366e93937e5b305fbb8aa9b48c712c3f8ed30e
Thomas1746 <fugethom@myvuw.ac.nz>
2016-04-21 13:00:05 +1200

    Add motor commands onto template

--- avc_code.c
+++ avc_code.c
@@ -10,8 +10,13 @@ extern "C" int sleep(int sec, int usec);
 extern "C" int write_digital(int chan, char level);
 extern "C" int set_motor(int motor, int dir, int speed);
 
+int speedL = 64;
+int speedR = 64;
+
 int main {
   init_hardware();
-  //
+  set_motor(1, 1, speedL);
+  set_motor(2, 2, speedR);
+  // sleep(3, 0) sleeps for 3 seconds
   return 0;
   }
