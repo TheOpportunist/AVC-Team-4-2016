d599635c97b0acd71ebaa10f4390b3aa1ccea898
Thomas1746 <fugethom@myvuw.ac.nz>
2016-05-12 13:00:26 +1200

    sent please to server

--- networking_code.cpp
+++ networking_code.cpp
@@ -13,10 +13,10 @@ int main (){
    //connects to server with the ip address 192.168.1.2
    connect_to_server("130.195.6.196", 1024);
    //sends a message to the connected server
-   send_to_server("Hello server");
+   send_to_server("Please");
    //receives message from the connected server
    char message[24];
    receive_from_server(message); //this may be buggy!
    printf("%s", message);
-
+   send_to_server(message);
 return 0;}
