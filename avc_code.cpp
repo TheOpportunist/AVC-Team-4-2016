2ac0ff523a6b9b2c6e91cf51f106d8bac68efcf9
Liam Byrne <liam.jb1@gmail.com>
2016-05-20 16:17:26 +1200

    Update avc_code.cpp

--- avc_code.cpp
+++ avc_code.cpp
@@ -62,7 +62,7 @@ return 0;
 */
 
   int sum=0;
-  int kp=255/600;
+  float kp=-255/600;
   int proportional_signal=0;
   int i, w, s;
   
@@ -96,9 +96,9 @@ return 0;
     proportional_signal = sum*kp;
     //if(proportional_signal<255){
     //set_motor(1, kp*proportional_signal);
-    set_motor(1, -(proportional_signal/(160*1*kp))*255);
+    set_motor(1, (proportional_signal/(160*1*kp))*255);
     //set_motor(2, kp*proportional_signal);
-    set_motor(2, -(proportional_signal/(160*1*kp))*255);
+    set_motor(2, (proportional_signal/(160*1*kp))*255);
     //}
     
     /**
