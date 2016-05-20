8202bf7e461801297310a6ad33bdff4cb7404292
Liam Byrne <liam.jb1@gmail.com>
2016-05-20 14:55:59 +1200

    Update avc_code.cpp

--- avc_code.cpp
+++ avc_code.cpp
@@ -70,12 +70,10 @@ int i, w, s;
     take_picture();
     printf("picture taken\n");
     
-    /**
     left = get_pixel(80, 120, 3);
     middle = get_pixel(160, 120, 3);
     right = get_pixel(240, 120, 3);
-    printf(" left=%d middle=%d right=%d \n", left, middle, right);
-    */
+    //printf(" left=%d middle=%d right=%d \n", left, middle, right);
     
     //int sum = 0;
     //for(i=0, i<320, i++) {
