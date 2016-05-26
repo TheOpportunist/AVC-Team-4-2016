d5b05e96b3268d05c807d6588b082f587ca7853e
Thomas1746 <fugethom@myvuw.ac.nz>
2016-05-26 14:35:50 +1200

    added if else statements for turning

--- pid_code.cpp
+++ pid_code.cpp
@@ -70,8 +70,16 @@ int main(){
     //set_motor(2, (v + (proportional_signal/(160*1*kp))*255 + derivative_signal));
     //set_motor(1, (v + (proportional_signal/(160*1*kp))*255));
     //set_motor(2, (v + (proportional_signal/(160*1*kp))*255));
-    set_motor(1, v + proportional_signal);
-    set_motor(2, v + proportional_signal);
+    if (proportional_signal == 0) {
+      set_motor(1, v + proportional_signal);
+      set_motor(2, v + proportional_signal);
+    } else if (proportional_signal > 0) {
+      set_motor(1, v);
+      set_motor(2, v + proportional_signal);
+    } else if (proportional_signal < 0) {
+      set_motor(1, v + proportional_signal);
+      set_motor(2, v);
+    }
   }
   return 0;
 }
