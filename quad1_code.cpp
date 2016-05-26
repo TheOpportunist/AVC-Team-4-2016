d6d705a45246624fc8099924bef58d65556f5697
Thomas1746 <fugethom@myvuw.ac.nz>
2016-05-27 09:37:45 +1200

    removed unnecessary code

--- quad1_code.cpp
+++ quad1_code.cpp
@@ -1,5 +1,3 @@
-// Code for AVC challenge
-
 #include <stdio.h>
 #include <time.h>
 #include <string.h>
@@ -41,22 +39,9 @@ extern "C" int receive_from_server(char message[24]);
 int main() {
   init(0);
   int i;
-  int left;
-  int right;
-  int middle;
-  int lDif;
-  int mDif;
-  int rDif;
+  int v_left = -150;
+  int v_right = -150;
   
-  /**
-  int v_left = 0;
-  int v_right = 0;
-  */
-  
-  int v_left = 180;
-  int v_right = 180;
-  
-  /**
    //connects to server with the ip address 192.168.1.2
    connect_to_server("130.195.6.196", 1024);
    //sends a message to the connected server
@@ -66,78 +51,20 @@ int main() {
    receive_from_server(message); //this may be buggy!
    printf("%s", message);
    send_to_server(message);
-  */
-
-  /**
-  int sum=0;
-  float kp=-255/600;
-  int proportional_signal=0;
-  int i, w, s;
-  */
   
   while(true) {
     take_picture();
     printf("picture taken\n");
     
-    left = get_pixel(80, 120, 3);
-    middle = get_pixel(160, 120, 3);
-    right = get_pixel(240, 120, 3);
-    printf(" left=%d middle=%d right=%d \n", left, middle, right);
+    //left = get_pixel(80, 120, 3);
+    //middle = get_pixel(160, 120, 3);
+    //right = get_pixel(240, 120, 3);
+    //printf(" left=%d middle=%d right=%d \n", left, middle, right);
     
-    //int sum = 0;
-    //for(i=0, i<320, i++) {
-    //white = get_pizel(120, i, 3)
-    //sum = sum + i*white;
-    //}
-    /**
-    for(i=0; i<320; i++){
-      //w=get_pixel(i,120,3);
-      w=(i-160)*get_pixel(i, 120, 3);
-      if(w>127){
-        s=1;
-      }
-      else{
-        s=0;
-      }
-    sum=sum+s;
-    }
-    
-    proportional_signal = sum*kp;
-    //if(proportional_signal<255){
-    //set_motor(1, kp*proportional_signal);
-    set_motor(1, (proportional_signal/(160*1*kp))*255);
-    //set_motor(2, kp*proportional_signal);
-    set_motor(2, (proportional_signal/(160*1*kp))*255);
-    //}
-    */
-    
-    if (middle > left && middle > right){
-      v_left = 180;
-      v_right = 180;
-      set_motor(1, -1*v_left);
-      set_motor(2, -1*v_right);
-    }
-    else if (right > middle && right > left) {
-      rDif = right - middle;
-      //v_left = 64;
-      //v_right = 128;
-      v_right = v_right + rDif;
-      set_motor(1, -1*v_left);
-      set_motor(2, -1*v_right);
-    }
-    else if (left > middle && left > right) {
-      lDif = middle - left;
-      //v_left = 128;
-      //v_right = 64;
-      v_left = v_left + lDif;
-      set_motor(1, -1*v_left);
-      set_motor(2, -1*v_right);
-    }
-    
+    set_motor(1, v_left);
+    set_motor(2, v_right);
     
     printf(" v_left=%d v_right=%d \n", v_left, v_right);
-    //Sleep(0,5);
-    //printf("%d\n", (proportional_signal/(160*1*kp))*255);
   }
   return 0;
 }
