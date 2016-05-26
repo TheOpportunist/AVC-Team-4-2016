7217cd6109583de648c143e4f1646240df14400a
Thomas1746 <fugethom@myvuw.ac.nz>
2016-05-27 10:09:59 +1200

    changed set_motor motor number

--- quad1_code.cpp
+++ quad1_code.cpp
@@ -39,7 +39,7 @@ extern "C" int receive_from_server(char message[24]);
 int main() {
   init(0);
   int i;
-  int v_left = -250;
+  int v_left = -210;
   int v_right = -210;
   
   /**
@@ -64,7 +64,7 @@ int main() {
     //printf(" left=%d middle=%d right=%d \n", left, middle, right);
     
     set_motor(1, v_left);
-    set_motor(1, v_right);
+    set_motor(2, v_right);
     
     printf(" v_left=%d v_right=%d \n", v_left, v_right);
   }
